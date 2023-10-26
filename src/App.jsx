3c3e60dbe5413a37aca3316fcc4b4f7dd53632b6
Patrick Selin <pat_devs@Patricks-MacBook-Pro.local>
2023-10-26 10:26:09 +0300

    fetch data fro firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+  
+
+  const fetchItems = () => {
+    fetch('https://todolist-4f300-default-rtdb.europe-west1.firebasedatabase.app/items/.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos(Object.values(data));
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
     <>
-      <p>
-        Hello World
-      </p>
+      <h1>Todo appp</h1>    
     </>
-  )
+  );
 }
-
-export default App
+export default App;
